835f0ed0561934bd7bb6f04e8b3cb57b25713d95
ggaranhani <gabrielgaranhani@gmail.com>
2020-12-10 13:22:29 -0300

    refactor: improve variable name in tests

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -13,17 +13,17 @@ describe('In memory User repository', () => {
     const users: UserData[] = []
     const name = 'any_name'
     const email = 'any@mail.com'
-    const userRepo = new InMemoryUserRepository(users)
-    await userRepo.add({ name, email })
-    const user = await userRepo.findUserByEmail('any@mail.com')
+    const sut = new InMemoryUserRepository(users)
+    await sut.add({ name, email })
+    const user = await sut.findUserByEmail('any@mail.com')
     expect(user.name).toBe('any_name')
   })
 
   test('should return all users in the repository', async () => {
     const users: UserData[] = [{ name: 'any_name', email: 'any@mail.com' },
       { name: 'second_name', email: 'second@mail.com' }]
-    const userRepo = new InMemoryUserRepository(users)
-    const returnedUsers = userRepo.findAllUsers()
+    const sut = new InMemoryUserRepository(users)
+    const returnedUsers = sut.findAllUsers()
     expect((await returnedUsers).length).toBe(2)
   })
 })
