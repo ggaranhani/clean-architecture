53e753021cd55c30cf953c51372e57b61d92850d
ggaranhani <gabrielgaranhani@gmail.com>
2021-02-04 08:07:27 -0300

    feat: controller should return 4000 when request is missing parameters

--- src/web-controllers/register-user-controller.ts
+++ src/web-controllers/register-user-controller.ts
@@ -1,7 +1,8 @@
 import { UserData } from '@/entities'
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
+import { MissingParamError } from './errors/missing-param-error'
 import { HttpRequest, HttpResponse } from './ports'
-import { created } from './util'
+import { badRequest, created } from './util'
 
 export class RegisterUserController {
   private readonly usecase: RegisterUserOnMailingList
@@ -11,11 +12,21 @@ export class RegisterUserController {
   }
 
   public async handle (request: HttpRequest): Promise<HttpResponse> {
+    if (!request.body.name || !request.body.email) {
+      let missingParam = !(request.body.name) ? 'name' : ''
+      missingParam += !(request.body.email) ? 'email' : ''
+      return badRequest(new MissingParamError(missingParam))
+    }
+
     const userData: UserData = request.body
     const response = await this.usecase.registerUserOnMailingList(userData)
 
     if (response.isRight()) {
       return created(response.value)
+    }
+
+    if (response.isLeft()) {
+      return badRequest(response.value)
     }
   }
 }
